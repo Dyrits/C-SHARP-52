b0aa621e1625d75f387c150c5e385123ea0a3f34
Dylan J. Gerrits <dyrits@outlook.com>
2024-10-09 10:25:05 +0000

    NumbersInCSharp added

---
+++ NumbersInCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿// Source: https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/numbers-in-csharp-local
+
+WorkWithIntegers();
+
+void WorkWithIntegers()
+{
+    int a = 18;
+    int b = 6;
+
+    // addition
+    int c = a + b;
+    Console.WriteLine(c);
+
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    // division
+    c = a / b;
+    Console.WriteLine(c);
+}
